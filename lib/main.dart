e0bf2f7f124df6b91c5211472d31b4f644ccd423
wanhasibala <wanhasibal1122@gmail.com>
2023-06-12 15:26:32 +0700

    Change background and centering text

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 200, 206, 221),
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    ),
+  );
 }
-
