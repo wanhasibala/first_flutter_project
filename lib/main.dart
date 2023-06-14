114a341332b429f83b46984d6b81defd4abffe49
wanhasibala <wanhasibal1122@gmail.com>
2023-06-14 09:41:33 +0700

    menanmbahkan styles pada background dan text

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,37 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 200, 206, 221),
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    MaterialApp(
+      home: Scaffold(body: GradientContainer()),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 70, 16, 133)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+          ),
+        ),
+      ),
+    );
+  }
 }
